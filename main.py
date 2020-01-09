57e40f60f01e7e0259eb5c0b85d0e2417774c696
Níkolas Vargas <vargasnikolass@gmail.com>
2020-01-09 00:31:38 -0300

    basic square moviments

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 """Import and initialize pygame lib"""
 import pygame
+from pygame import Surface
+from pygame.sprite import Sprite
 from pygame.locals import (
     K_UP,
     K_DOWN,
@@ -12,35 +14,62 @@ from pygame.locals import (
     QUIT
 )
 
+SCREEN_WIDTH: int = 800
+SCREEN_HEIGHT: int = 600
+
+
+class Player(Sprite):
+    def __init__(self) -> None:
+        super().__init__()
+        self.surf = Surface((75, 75))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
+        self.allowed_moviments = [K_UP, K_DOWN, K_LEFT, K_RIGHT]
+        self.window_bounds = 0
+
+    def update(self, key: dict) -> None:
+        if key[K_UP]:
+            self.rect.move_ip(0, -5)
+        if key[K_DOWN]:
+            self.rect.move_ip(0, 5)
+        if key[K_LEFT]:
+            self.rect.move_ip(-5, 0)
+        if key[K_RIGHT]:
+            self.rect.move_ip(5, 0)
+
+        if self.rect.left < self.window_bounds:
+            self.rect.left = self.window_bounds
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+        if self.rect.top <= self.window_bounds:
+            self.rect.top = self.window_bounds
+        if self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
+
+
 pygame.init()
+screen: Surface = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_HEIGHT))
 
-SCREEN_WIDTH, SCREEN_HEIGHT = (800, 600)
-screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_HEIGHT))
+player = Player()
+running: bool = True
 
-running = True
 while running:
     for event in pygame.event.get():
-        if event.type == KEYDOWN:
-            if event.key == K_ESCAPE:
-                running = False
-        elif event.type == QUIT:
+        exit_key: bool = (
+            event.type == QUIT or
+            event.type == KEYDOWN and event.key == K_ESCAPE
+        )
+        if exit_key:
             running = False
+
+    key_pressed = pygame.key.get_pressed()
+    player.update(key_pressed)
+
     # Fill the background with white color
-    screen.fill((255, 255, 255))
+    screen.fill((0, 0, 0))
 
-    surf = pygame.Surface((50, 50))
-    surf.fill((0, 0, 0))
-    rect = surf.get_rect()
+    screen.blit(player.surf, player.rect)
 
-    surf_center = (
-        (SCREEN_WIDTH - surf.get_width()) / 2,
-        (SCREEN_HEIGHT - surf.get_height()) / 2,
-    )
-
-    screen.blit(surf, surf_center)
-
-    # pygame.draw.circle(screen, (0, 0 ,255), (250, 250), 75)
     pygame.display.flip()
 
 pygame.quit()
-
