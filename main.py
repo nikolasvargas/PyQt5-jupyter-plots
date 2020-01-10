1617e2b0a1f9194c23bf59e4a2e4443f7c6e2ee4
Níkolas Vargas <vargasnikolass@gmail.com>
2020-01-10 00:40:33 -0300

    added cloud event

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 import pygame
 from pygame.locals import K_ESCAPE, KEYDOWN, QUIT
 from chars import Player, Enemy
+from scenery import Cloud
 from configparser import ConfigParser
 
 cfg = ConfigParser()
@@ -14,12 +15,16 @@ SCREEN_HEIGHT = int(cfg['DEFAULT']['SCREEN_HEIGHT'])
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
 
 ADD_ENEMY = pygame.USEREVENT + 1
 pygame.time.set_timer(ADD_ENEMY, 250)
+ADD_CLOUD = pygame.USEREVENT + 2
+pygame.time.set_timer(ADD_CLOUD, 1000)
 
 player = Player()
 enemies = pygame.sprite.Group()
+clouds = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
 all_sprites.add(player)
 
@@ -37,13 +42,18 @@ while running:
             new_enemy = Enemy()
             enemies.add(new_enemy)
             all_sprites.add(new_enemy)
+        elif event.type == ADD_CLOUD:
+            new_cloud = Cloud()
+            enemies.add(new_cloud)
+            all_sprites.add(new_cloud)
 
     key_pressed = pygame.key.get_pressed()
     player.update(key_pressed)
     enemies.update()
+    clouds.update()
 
     # Fill the background with white color
-    screen.fill((0, 0, 0))
+    screen.fill((135, 206, 250))
 
     for entity in all_sprites:
         screen.blit(entity.surf, entity.rect)
@@ -53,5 +63,6 @@ while running:
         running = False
 
     pygame.display.flip()
+    clock.tick(30)
 
 pygame.quit()
